d234cb184b699cf9b6b8ec5b9b3b35c96a65a47c
Arabic Source <115221367+Tepthonee@users.noreply.github.com>
2023-01-24 20:25:30 +0300

    Update الفحص.py

--- sbb_b/plugins/الفحص.py
+++ sbb_b/plugins/الفحص.py
@@ -1,22 +1,21 @@
 import random
-import re
 import time
 from datetime import datetime
 from platform import python_version
 
 import requests
-from telethon import version, Button, events
+from telethon import Button, events, version
 from telethon.errors.rpcerrorlist import (
     MediaEmptyError,
     WebpageCurlFailedError,
     WebpageMediaEmptyError,
 )
-from telethon.events import CallbackQuery
+
 from sbb_b import StartTime, jmthonversion, sbb_b
 
 from ..Config import Config
 from ..core.managers import edit_or_reply
-from ..helpers.functions import check_data_base_heal_th, get_readable_time, jmthonalive
+from ..helpers.functions import check_data_base_heal_th, get_readable_time
 from ..helpers.utils import reply_id
 from ..sql_helper.globals import gvarstatus
 from . import mention
@@ -32,12 +31,14 @@ async def amireallyalive(event):
         ANIME = f"**“{data['quote']}” - {data['character']} ({data['anime']})**"
     uptime = await get_readable_time((time.time() - StartTime))
     start = datetime.now()
-    jmthonevent = await edit_or_reply(event, "**᯽︙ يتـم التـأكـد انتـظر قليلا رجاءً**")
+    jmthonevent = await edit_or_reply(event, "** ᯽︙ يتـم التـأكـد انتـظر قليلا رجاءً**")
     end = datetime.now()
     ms = (end - start).microseconds / 1000
     _, check_sgnirts = check_data_base_heal_th()
-    EMOJI = gvarstatus("ALIVE_EMOJI") or "⿻┊‌‎"
-    ALIVE_TEXT = gvarstatus("ALIVE_TEXT") or "**父[ 𝚃‌𝙴𝙿𝚃𝙷𝙾𝙽‌ 𝙸𝚂 𝚆𝙾𝚁𝙺𝙸𝙽𝙶 ✓ ](t.me/Tepthon)父**"
+    EMOJI = gvarstatus("ALIVE_EMOJI") or "⿻┊‌"
+    ALIVE_TEXT = (
+        gvarstatus("ALIVE_TEXT") or "**父[ 𝚃𝙴𝙿𝚃𝙷𝙾𝙽 𝙸𝚂 𝚆𝙾𝚁𝙺𝙸𝙽𝙶 ✓ ](t.me/Tepthon)父**"
+    )
     JMTHON_IMG = gvarstatus("ALIVE_PIC")
     caption = jmthon_caption.format(
         ALIVE_TEXT=ALIVE_TEXT,
@@ -46,7 +47,7 @@ async def amireallyalive(event):
         mention=mention,
         uptime=uptime,
         telever=version.__version__,
-        jmver=jmthonversion,
+        repver=jmthonversion,
         pyver=python_version(),
         dbhealth=check_sgnirts,
         ping=ms,
@@ -74,15 +75,15 @@ async def amireallyalive(event):
 temp = """{ALIVE_TEXT}
 **‎{EMOJI}‌‎𝙽𝙰𝙼𝙴 𖠄 {mention}** ٫
 **‌‎{EMOJI}‌‎𝙿𝚈𝚃𝙷𝙾𝙽 𖠄 {pyver}** ٫
-**‌‎{EMOJI}‌‎𝚃‌𝙴𝙿𝚃𝙷𝙾𝙽‌ 𖠄 {telever}** ٫
+**‌‎{EMOJI}‌‎𝚃𝙴𝙿𝚃𝙷𝙾𝙽 𖠄 {telever}** ٫
 **‌‎{EMOJI}‌‎𝚄𝙿𝚃𝙸𝙼𝙴 𖠄 {uptime}** ٫
 ‌‎**{EMOJI}‌‎‌‎𝙿𝙸𝙽𝙶 𖠄 {ping}** ٫
-**𖠄 Tepthon 𝘂𝘀𝗲𝗿𝗯𝗼𝘁 𖠄**"""
+**𖠄 𝗧𝗘𝗣𝗧𝗛𝗢𝗡 𝘂𝘀𝗲𝗿𝗯𝗼𝘁 𖠄**"""
 
 
 def jmthonalive_text():
     EMOJI = gvarstatus("ALIVE_EMOJI") or "  ✥ "
-    jmthon_caption = "**𝚃‌𝙴𝙿𝚃𝙷𝙾𝙽‌ 𝙸𝚂 𝚆𝙾𝚁𝙺𝙸𝙽𝙶**\n"
+    jmthon_caption = "**سورس تيبثون يعمل بنجاح**\n"
     jmthon_caption += f"**{EMOJI} اصدار التيليثون :** `{version.__version__}\n`"
     jmthon_caption += f"**{EMOJI} اصدار تيبثون :** `{jmthonversion}`\n"
     jmthon_caption += f"**{EMOJI} اصدار البايثون :** `{python_version()}\n`"
@@ -90,20 +91,20 @@ def jmthonalive_text():
     return jmthon_caption
 
 
-
 @sbb_b.ar_cmd(pattern="السورس$")
 async def repo(event):
     RR7PP = Config.TG_BOT_USERNAME
     if event.reply_to_msg_id:
         await event.get_reply_message()
-    response = await sbb_b.inline_query(PPF22, "السورس")
+    response = await sbb_b.inline_query(RR7PP, "السورس")
     await response[0].click(event.chat_id)
     await event.delete()
 
-ROZ_PIC = "https://telegra.ph/file/577bd6562f22ca288f645.jpg"
+
+ROZ_PIC = "https://telegra.ph/file/8e6c848dc65b4ef693d0d.jpg"
 RAZAN = Config.TG_BOT_USERNAME
 ROZ_T = (
-    f"**⌯︙بوت تيبثـون يعمل بنجاح 🤍،**\n"
+    f"**⌯︙بوت تيبثون يعمل بنجاح 🤍،**\n"
     f"**   - اصدار التليثون :** `1.23.0\n`"
     f"**   - اصدار تيبثون :** `4.0.0`\n"
     f"**   - البوت المستخدم :** `{RAZAN}`\n"
@@ -122,7 +123,7 @@ if Config.TG_BOT_USERNAME is not None and tgbot is not None:
         if query.startswith("السورس") and event.query.user_id == sbb_b.uid:
             buttons = [
                 [
-                    Button.url("قنـاة السـورس ⚙️", "https://t.me/Tepthone"),
+                    Button.url("قنـاة السـورس ⚙️", "https://t.me/Tepthon"),
                     Button.url("المطـور 👨🏼‍💻", "https://t.me/PPF22"),
                 ]
             ]
@@ -146,8 +147,6 @@ if Config.TG_BOT_USERNAME is not None and tgbot is not None:
                     link_preview=False,
                 )
             await event.answer([result] if result else None)
-
-
 
 
 # edit by ~ @PPF22
