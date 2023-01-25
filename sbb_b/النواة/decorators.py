a910a589ec81cc070cbbc11e913a94bad92f377e
Hmd Mostafa <98346135+xxe5@users.noreply.github.com>
2023-01-26 01:28:17 +0300

    Update and rename sbb_b/core/decorators.py to sbb_b/النواة/decorators.py

--- sbb_b/النواة/decorators.py
+++ sbb_b/النواة/decorators.py
@@ -22,7 +22,7 @@ def check_owner(func):
         else:
             HELP_TEXT = (
                 gvarstatus("HELP_TEXT")
-                or "فقط مالك البوت له صلاحيات الدخول هنا\n\n@Tepthonee"
+                or "فقط مالك البوت له صلاحيات الدخول هنا\n\n@Tepthon"
             )
             await c_q.answer(
                 HELP_TEXT,
