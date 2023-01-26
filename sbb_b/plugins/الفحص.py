f45de961cd8948b199880d8489c902edd5d25e8f
Hmd Mostafa <98346135+xxe5@users.noreply.github.com>
2023-01-26 16:01:17 +0300

    Update الفحص.py

--- sbb_b/plugins/الفحص.py
+++ sbb_b/plugins/الفحص.py
@@ -1,21 +1,22 @@
 import random
+import re
 import time
 from datetime import datetime
 from platform import python_version
 
 import requests
-from telethon import Button, events, version
+from telethon import version, Button, events
 from telethon.errors.rpcerrorlist import (
     MediaEmptyError,
     WebpageCurlFailedError,
     WebpageMediaEmptyError,
 )
-
-from sbb_b import StartTime, tepthonversion, sbb_b
+from telethon.events import CallbackQuery
+from sbb_b import StartTime, jmthonversion, sbb_b
 
 from ..Config import Config
 from ..core.managers import edit_or_reply
-from ..helpers.functions import check_data_base_heal_th, get_readable_time
+from ..helpers.functions import check_data_base_heal_th, get_readable_time, jmthonalive
 from ..helpers.utils import reply_id
 from ..sql_helper.globals import gvarstatus
 from . import mention
@@ -25,27 +26,27 @@ from . import mention
 async def amireallyalive(event):
     reply_to_id = await reply_id(event)
     ANIME = None
-    tepthon_caption = gvarstatus("ALIVE_TEMPLATE") or temp
-    if "ANIME" in tepthon_caption:
+    jmthon_caption = gvarstatus("ALIVE_TEMPLATE") or temp
+    if "ANIME" in jmthon_caption:
         data = requests.get("https://animechan.vercel.app/api/random").json()
         ANIME = f"**“{data['quote']}” - {data['character']} ({data['anime']})**"
     uptime = await get_readable_time((time.time() - StartTime))
     start = datetime.now()
-    tepthonevent = await edit_or_reply(event, "**᯽︙ يتـم التـأكـد انتـظر قليلا رجاءً**")
+    jmthonevent = await edit_or_reply(event, "**᯽︙ يتـم التـأكـد انتـظر قليلا رجاءً**")
     end = datetime.now()
     ms = (end - start).microseconds / 1000
     _, check_sgnirts = check_data_base_heal_th()
-    EMOJI = gvarstatus("ALIVE_EMOJI") or "‌‎⿻┊‌‎‌‎"
-    ALIVE_TEXT = gvarstatus("ALIVE_TEXT") or "**父 𝚃‌𝙴𝙿𝚃𝙷𝙾𝙽‌ 𝙸𝚂 𝚆𝙾𝚁𝙺𝙸𝙽𝙶 ✓ 父 (t.me/Tepthon)**"
+    EMOJI = gvarstatus("ALIVE_EMOJI") or "⿻┊‌‎"
+    ALIVE_TEXT = gvarstatus("ALIVE_TEXT") or "**父[ 𝚃‌𝙴𝙿𝚃𝙷𝙾𝙽‌ 𝙸𝚂 𝚆𝙾𝚁𝙺𝙸𝙽𝙶 ✓ ](t.me/Tepthone)父**"
     JMTHON_IMG = gvarstatus("ALIVE_PIC")
-    caption = tepthon_caption.format(
+    caption = jmthon_caption.format(
         ALIVE_TEXT=ALIVE_TEXT,
         ANIME=ANIME,
         EMOJI=EMOJI,
         mention=mention,
         uptime=uptime,
         telever=version.__version__,
-        tepver=tepthonversion,
+        tepver=jmthonversion,
         pyver=python_version(),
         dbhealth=check_sgnirts,
         ping=ms,
@@ -57,36 +58,37 @@ async def amireallyalive(event):
             await event.client.send_file(
                 event.chat_id, PIC, caption=caption, reply_to=reply_to_id
             )
-            await tepthonevent.delete()
+            await jmthonevent.delete()
         except (WebpageMediaEmptyError, MediaEmptyError, WebpageCurlFailedError):
             return await edit_or_reply(
-                tepthonevent,
+                jmthonevent,
                 f"**رابط الصورة غير صحيح**\nعليك الرد على رابط الصورة ب .اضف صورة الفحص",
             )
     else:
         await edit_or_reply(
-            tepthonevent,
+            jmthonevent,
             caption,
         )
 
 
 temp = """{ALIVE_TEXT}
-**{EMOJI} 𝙳𝙰𝚃𝙰𝙱𝙰𝚂𝙴 𖠄 :** `{dbhealth}`
-**{EMOJI} 𝚃𝙴𝙻𝙴𝚃𝙷𝙾𝙽 𖠄:** `{telever}`
-**{EMOJI} 𝚃𝙴𝙿𝚃𝙷𝙾𝙽 𖠄 :** `{tepver}`
-**{EMOJI} 𝙿𝚈𝚃𝙷𝙾𝙽 𖠄 :** `{pyver}`
-**{EMOJI} 𝚄𝙿𝚃𝙸𝙼𝙴 𖠄 :** `{uptime}`
-**{EMOJI} 𝙽𝙰𝙼𝙴 𖠄:** {mention}"""
+**‎{EMOJI}‌‎𝙽𝙰𝙼𝙴 𖠄 {mention}** ٫
+**‌‎{EMOJI}‌‎𝙿𝚈𝚃𝙷𝙾𝙽 𖠄 {pyver}** ٫
+**‌‎{EMOJI}‌‎𝚃‌𝙴𝙿𝚃𝙷𝙾𝙽‌ 𖠄 {telever}** ٫
+**‌‎{EMOJI}‌‎𝚄𝙿𝚃𝙸𝙼𝙴 𖠄 {uptime}** ٫
+‌‎**{EMOJI}‌‎‌‎𝙿𝙸𝙽𝙶 𖠄 {ping}** ٫
+**𖠄 Tepthon 𝘂𝘀𝗲𝗿𝗯𝗼𝘁 𖠄**"""
 
 
 def jmthonalive_text():
     EMOJI = gvarstatus("ALIVE_EMOJI") or "  ✥ "
-    tepthon_caption = "**سورس تيبثون يعمل بنجاح**\n"
-    tepthon_caption += f"**{EMOJI} اصدار التيليثون :** `{version.__version__}\n`"
-    tepthon_caption += f"**{EMOJI} اصدار تيبثون :** `{tepthonversion}`\n"
-    tepthon_caption += f"**{EMOJI} اصدار البايثون :** `{python_version()}\n`"
-    tepthon_caption += f"**{EMOJI} المالك:** {mention}\n"
-    return tepthon_caption
+    jmthon_caption = "**𝚃‌𝙴𝙿𝚃𝙷𝙾𝙽‌ 𝙸𝚂 𝚆𝙾𝚁𝙺𝙸𝙽𝙶**\n"
+    jmthon_caption += f"**{EMOJI} اصدار التيليثون :** `{version.__version__}\n`"
+    jmthon_caption += f"**{EMOJI} اصدار تيبثون :** `{jmthonversion}`\n"
+    jmthon_caption += f"**{EMOJI} اصدار البايثون :** `{python_version()}\n`"
+    jmthon_caption += f"**{EMOJI} المالك:** {mention}\n"
+    return jmthon_caption
+
 
 
 @sbb_b.ar_cmd(pattern="السورس$")
@@ -94,15 +96,14 @@ async def repo(event):
     RR7PP = Config.TG_BOT_USERNAME
     if event.reply_to_msg_id:
         await event.get_reply_message()
-    response = await sbb_b.inline_query(RR7PP, "السورس")
+    response = await sbb_b.inline_query(PPF22, "السورس")
     await response[0].click(event.chat_id)
     await event.delete()
 
-
-ROZ_PIC = "https://graph.org/file/958276a82dde1bdc180f4.jpg"
+ROZ_PIC = "https://telegra.ph/file/577bd6562f22ca288f645.jpg"
 RAZAN = Config.TG_BOT_USERNAME
 ROZ_T = (
-    f"**⌯︙بوت تيبثون يعمل بنجاح 🤍،**\n"
+    f"**⌯︙بوت تيبثـون يعمل بنجاح 🤍،**\n"
     f"**   - اصدار التليثون :** `1.23.0\n`"
     f"**   - اصدار تيبثون :** `4.0.0`\n"
     f"**   - البوت المستخدم :** `{RAZAN}`\n"
@@ -121,7 +122,7 @@ if Config.TG_BOT_USERNAME is not None and tgbot is not None:
         if query.startswith("السورس") and event.query.user_id == sbb_b.uid:
             buttons = [
                 [
-                    Button.url("قنـاة السـورس ⚙️", "https://t.me/Tepthon"),
+                    Button.url("قنـاة السـورس ⚙️", "https://t.me/Tepthone"),
                     Button.url("المطـور 👨🏼‍💻", "https://t.me/PPF22"),
                 ]
             ]
@@ -145,6 +146,8 @@ if Config.TG_BOT_USERNAME is not None and tgbot is not None:
                     link_preview=False,
                 )
             await event.answer([result] if result else None)
+
+
 
 
 # edit by ~ @PPF22
