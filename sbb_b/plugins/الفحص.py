e235224e7c665d3257102d356421a4eb951e737a
Hmd Mostafa <98346135+xxe5@users.noreply.github.com>
2023-01-26 15:40:04 +0300

    Update الفحص.py

--- sbb_b/plugins/الفحص.py
+++ sbb_b/plugins/الفحص.py
@@ -11,7 +11,7 @@ from telethon.errors.rpcerrorlist import (
     WebpageMediaEmptyError,
 )
 
-from sbb_b import StartTime, jmthonversion, sbb_b
+from sbb_b import StartTime, tepthonversion, sbb_b
 
 from ..Config import Config
 from ..core.managers import edit_or_reply
@@ -25,47 +25,47 @@ from . import mention
 async def amireallyalive(event):
     reply_to_id = await reply_id(event)
     ANIME = None
-    jmthon_caption = gvarstatus("ALIVE_TEMPLATE") or temp
-    if "ANIME" in jmthon_caption:
+    tepthon_caption = gvarstatus("ALIVE_TEMPLATE") or temp
+    if "ANIME" in tepthon_caption:
         data = requests.get("https://animechan.vercel.app/api/random").json()
         ANIME = f"**“{data['quote']}” - {data['character']} ({data['anime']})**"
     uptime = await get_readable_time((time.time() - StartTime))
     start = datetime.now()
-    jmthonevent = await edit_or_reply(event, "**᯽︙ يتـم التـأكـد انتـظر قليلا رجاءً**")
+    tepthonevent = await edit_or_reply(event, "**᯽︙ يتـم التـأكـد انتـظر قليلا رجاءً**")
     end = datetime.now()
     ms = (end - start).microseconds / 1000
     _, check_sgnirts = check_data_base_heal_th()
     EMOJI = gvarstatus("ALIVE_EMOJI") or "‌‎⿻┊‌‎‌‎"
     ALIVE_TEXT = gvarstatus("ALIVE_TEXT") or "**父 𝚃‌𝙴𝙿𝚃𝙷𝙾𝙽‌ 𝙸𝚂 𝚆𝙾𝚁𝙺𝙸𝙽𝙶 ✓ 父 (t.me/Tepthon)**"
-    JMTHON_IMG = gvarstatus("ALIVE_PIC")
-    caption = jmthon_caption.format(
+    tepthon_IMG = gvarstatus("ALIVE_PIC")
+    caption = tepthon_caption.format(
         ALIVE_TEXT=ALIVE_TEXT,
         ANIME=ANIME,
         EMOJI=EMOJI,
         mention=mention,
         uptime=uptime,
         telever=version.__version__,
-        jmver=jmthonversion,
+        tepver=tepthonversion,
         pyver=python_version(),
         dbhealth=check_sgnirts,
         ping=ms,
     )
-    if JMTHON_IMG:
-        JMTHON = list(JMTHON_IMG.split())
-        PIC = random.choice(JMTHON)
+    if tepthon_IMG:
+        tepthon = list(tepthon_IMG.split())
+        PIC = random.choice(tepthon)
         try:
             await event.client.send_file(
                 event.chat_id, PIC, caption=caption, reply_to=reply_to_id
             )
-            await jmthonevent.delete()
+            await tepthonevent.delete()
         except (WebpageMediaEmptyError, MediaEmptyError, WebpageCurlFailedError):
             return await edit_or_reply(
-                jmthonevent,
+                tepthonevent,
                 f"**رابط الصورة غير صحيح**\nعليك الرد على رابط الصورة ب .اضف صورة الفحص",
             )
     else:
         await edit_or_reply(
-            jmthonevent,
+            tepthonevent,
             caption,
         )
 
@@ -73,28 +73,28 @@ async def amireallyalive(event):
 temp = """{ALIVE_TEXT}
 **{EMOJI} 𝙳𝙰𝚃𝙰𝙱𝙰𝚂𝙴 𖠄 :** `{dbhealth}`
 **{EMOJI} 𝚃𝙴𝙻𝙴𝚃𝙷𝙾𝙽 𖠄:** `{telever}`
-**{EMOJI} 𝚃𝙴𝙿𝚃𝙷𝙾𝙽 𖠄 :** `{jmver}`
+**{EMOJI} 𝚃𝙴𝙿𝚃𝙷𝙾𝙽 𖠄 :** `{tepver}`
 **{EMOJI} 𝙿𝚈𝚃𝙷𝙾𝙽 𖠄 :** `{pyver}`
 **{EMOJI} 𝚄𝙿𝚃𝙸𝙼𝙴 𖠄 :** `{uptime}`
 **{EMOJI} 𝙽𝙰𝙼𝙴 𖠄:** {mention}"""
 
 
-def jmthonalive_text():
+def tepthonalive_text():
     EMOJI = gvarstatus("ALIVE_EMOJI") or "  ✥ "
-    jmthon_caption = "**سورس تيبثون يعمل بنجاح**\n"
-    jmthon_caption += f"**{EMOJI} اصدار التيليثون :** `{version.__version__}\n`"
-    jmthon_caption += f"**{EMOJI} اصدار تيبثون :** `{jmthonversion}`\n"
-    jmthon_caption += f"**{EMOJI} اصدار البايثون :** `{python_version()}\n`"
-    jmthon_caption += f"**{EMOJI} المالك:** {mention}\n"
-    return jmthon_caption
+    tepthon_caption = "**سورس تيبثون يعمل بنجاح**\n"
+    tepthon_caption += f"**{EMOJI} اصدار التيليثون :** `{version.__version__}\n`"
+    tepthon_caption += f"**{EMOJI} اصدار تيبثون :** `{tepthonversion}`\n"
+    tepthon_caption += f"**{EMOJI} اصدار البايثون :** `{python_version()}\n`"
+    tepthon_caption += f"**{EMOJI} المالك:** {mention}\n"
+    return tepthon_caption
 
 
 @sbb_b.ar_cmd(pattern="السورس$")
 async def repo(event):
-    RR7PP = Config.TG_BOT_USERNAME
+    PPF22 = Config.TG_BOT_USERNAME
     if event.reply_to_msg_id:
         await event.get_reply_message()
-    response = await sbb_b.inline_query(RR7PP, "السورس")
+    response = await sbb_b.inline_query(PPF22, "السورس")
     await response[0].click(event.chat_id)
     await event.delete()
 
